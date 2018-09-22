deafb4c168e270e9a607a03bc520c90d8e161bb1
walerij <becwal@yandex.ru>
2018-09-22 10:40:11 +0300

    вывод данных в базу

--- dbase.php
+++ dbase.php
@@ -11,18 +11,49 @@ class db
       
         }
     
-        
-        function foot_query($sql_text="SELECT NOW()")
+        //получение ответа на SELECT-запрос
+        public function foot_query($sql_text="SELECT NOW()")
         { 
             $res=mysqli_query($this->sql, $sql_text);
-            $row=mysqli_fetch_array($res);
-            return $row;
+            //$row=mysqli_fetch_assoc($res);
+            //    var_dump($row);
+            return $res;
 
+        }
+    
+        //формирование таблицы вывода
+        public function get_table($res)
+        {
+            
+            
+            $tmp_table="<table cellpadding='5' cellspacing='0' border='1'>";
+            //заголовки таблицы
+            $tmp_table.= "<tr>";
+            $i = $res->fetch_fields();
+                foreach ($i as $val)
+                    $tmp_table.= '<th>'.$val->name.'</th>';
+                  
+            $tmp_table.= "</tr>";
+            //данные таблицы
+            while ($result = mysqli_fetch_array($res, MYSQLI_NUM))
+            { 
+                    $tmp_table.= "<tr>";
+                  
+                    foreach ($result as  $value ) 
+                       $tmp_table.= "<td>".$value."</td>";
+                    
+                   
+                    $tmp_table.= "</tr>";                
+            }
+                
+            $tmp_table.= "</table>";
+            
+            return $tmp_table;
         }
 
     
-    
 }
 
-$d=new db();
-var_dump($d->foot_query("SELECT * FROM player LIMIT 5"));
+$database = new db();
+$foot_res = $database->foot_query("SELECT * FROM player LIMIT 5");
+echo $database->get_table($foot_res);
